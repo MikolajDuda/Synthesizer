231de349ebcc18392c0dbbba6d844fb38b8654c9
MikolajDuda <duda.mikoaj@gmail.com>
2020-04-04 23:55:10 +0200

    Fix sawtooth waveForm generation, add function getAmplitude
    getAmplitude is simple envelope-like function which gradually mutes
    waves.
    
    Add getAmplitude to all waveForms generation.

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,20 @@
 public class Main {
-    public static void main(String[] args){
+    public static void main(String[] args) {
 
-        byte[] wave = WaveMaker.getWave(WaveMaker.SINE, 440);
-
+        byte[] wave = WaveMaker.getWave(WaveMaker.SAWTOOTH, 440);
         SoundMaker.playWave(wave);
 
+        byte[] wave2 = WaveMaker.getWave(WaveMaker.SINE, 440);
+        SoundMaker.playWave(wave2);
+
+        byte[] wave3 = WaveMaker.getWave(WaveMaker.SQUARE, 440);
+        SoundMaker.playWave(wave3);
+
+        byte[] wave4 = WaveMaker.getWave(WaveMaker.TRIANGLE, 440);
+        SoundMaker.playWave(wave4);
+
+
+
     }
+
 }
--- src/WaveMaker.java
+++ src/WaveMaker.java
@@ -5,65 +5,89 @@ public class WaveMaker {
     public static final int SAWTOOTH = 3;
 
     public static int sampleRate = 44100;
-    public static double amplitude = 0.4;
-    public static int seconds = 1;
+    public static double amplitude = 1;
+    public static int time = 5;           // duration of sound
 
     public static byte[] getWave(int waveForm, double frequency) {
 
-        byte[] wave = new byte[3 * seconds * sampleRate];   // 3 * zeby czas w seconds zgadza sie wtedy, kiedy AudioFormat w SoundMakerze ma sampleSizeInBits = 16
+        byte[] wave = new byte[time * sampleRate];
 
         double samplingInterval = sampleRate / frequency;
 
-        switch(waveForm){
+        switch (waveForm) {
             default:
                 for (int i = 0; i < wave.length; i++) {
-                    wave[i] = (byte) (0 * 127);
+                    wave[i] = (byte) 0;
                 }
                 break;
-                
+
             case SINE:
                 for (int i = 0; i < wave.length; i++) {
-                    double angle = (2.0 * Math.PI * i) / (2 * samplingInterval);  // 2 * wtedy, kiedy AudioFormat w SoundMakerze ma sampleSizeInBits = 16
-                    wave[i] = (byte) (amplitude * Math.sin(angle) * 127);
+                    amplitude = getAmplitude(i, wave.length);
+                    double angle = (2.0 * Math.PI * i) / (2 * samplingInterval);
+                    wave[i] = (byte) (amplitude * Math.sin(angle) * Byte.MAX_VALUE);
                 }
                 break;
 
             case SQUARE:
                 for (int i = 0; i < wave.length; i++) {
+                    amplitude = getAmplitude(i, wave.length);
                     double angle = (2.0 * Math.PI * i) / (2 * samplingInterval);
 
                     if (Math.sin(angle) >= 0.0)
-                        wave[i] = (byte) (amplitude * 0.3 * 127);
+                        wave[i] = (byte) (amplitude * 0.2 * Byte.MAX_VALUE);
                     else
-                        wave[i] = (byte) (amplitude * -0.3 * 128);
+                        wave[i] = (byte) (amplitude * 0.2 * Byte.MIN_VALUE);
                 }
                 break;
 
             case TRIANGLE:
                 for (int i = 0; i < wave.length; i++) {
+                    amplitude = getAmplitude(i, wave.length);
                     double angle = (2.0 * Math.PI * i) / (2 * samplingInterval);
-                    wave[i] = (byte) ((amplitude * (2.0 / Math.PI) * Math.asin(Math.sin(angle)) * 127));
+                    wave[i] = (byte) ((amplitude * (1.4 / Math.PI) * Math.asin(Math.sin(angle)) * Byte.MAX_VALUE));
                 }
                 break;
 
-            case SAWTOOTH:  //TODO: spraw, aby sawtooth dzialal!
+            case SAWTOOTH:
                 for (int i = 0; i < wave.length; i++) {
+
+                    amplitude = getAmplitude(i, wave.length);
+
                     double angle = (2.0 * Math.PI * i) / (2 * samplingInterval);
-                    wave[i] = (byte) (amplitude * 0.6 * (2.0 / Math.PI * (angle * Math.PI * (i%(1.0/angle))) -Math.PI/2.0) * 127);
+                    // Adding subsequent sines to create sawtooth wave
+                    for (int j = 1; j < 35; j++) {
+                        wave[i] += (byte) (0.7 * amplitude / Math.PI * -Math.sin(j * angle) / j * Byte.MAX_VALUE);
+                    }
                 }
                 break;
 
         }
-
+        System.out.println(wave.length);
+        amplitude = 1;
         return wave;
 
     }
 
-    public static void envelope() {
 
-    }
+    public static double getAmplitude(int i, int waveLength) {
 
-    public static double getAmplitude() {
+        if (i >= (0.2 * waveLength) && i < (0.6 * waveLength)) {
+            amplitude -= 0.5 * amplitude / waveLength;
+        }
+        if (i >= (0.6 * waveLength) && i < (0.8 * waveLength)) {
+            amplitude -= 6 * amplitude / waveLength;
+            if (amplitude == 0) {
+                amplitude = 0;
+            }
+        }
+        if (i >= 0.8 * waveLength) {
+            amplitude -= 150 * amplitude / waveLength;
+            if (amplitude == 0) {
+                amplitude = 0;
+            }
+        }
+
         return amplitude;
     }
 
