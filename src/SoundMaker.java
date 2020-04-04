1b4818c6ee635bb7e9166bce83615677880e70a5
MikolajDuda <duda.mikoaj@gmail.com>
2020-04-04 21:26:32 +0200

    Delete unnecessary lines and comments, format code

--- src/SoundMaker.java
+++ src/SoundMaker.java
@@ -4,9 +4,7 @@ import javax.sound.sampled.SourceDataLine;
 
 
 public class SoundMaker {
-    private static int sampleRate = 44100;   //must be >= 4000
-    private static int size = 32768;         //buffer size = 2^8
-
+    private static int sampleRate = 44100;
 
     public static void playWave(byte[] wave) {
         final AudioFormat af = new AudioFormat(sampleRate, 16, 1, true, true);
@@ -26,29 +24,8 @@ public class SoundMaker {
         }
     }
 
-    private static void writeToLine(SourceDataLine line, byte[] buffer) {               //TODO: zrobic ladne play()
-        //int length = sampleRate * array.length / 1000;
+    private static void writeToLine(SourceDataLine line, byte[] buffer) {
         line.write(buffer, 0, buffer.length);
     }
-/*
 
-    private static byte[] generateSine(double frequencyOfSignal, int seconds) {     //TODO: wywalic generateSine
-        // total samples = (duration in second) * (samples per second) * 2
-        byte[] sin = new byte[2 * seconds * sampleRate];
-        double samplingInterval = (double) (sampleRate / (2 * frequencyOfSignal));
-        System.out.println("Sampling Frequency  : " + sampleRate);
-        System.out.println("Frequency of Signal : " + frequencyOfSignal);
-        System.out.println("Sampling Interval   : " + samplingInterval);
-
-        for (int i = 0; i < sin.length; i++) {
-            double angle = (2.0 * Math.PI * i) /(2 * samplingInterval);
-            sin[i] = (byte) ((amplitude * Math.sin(angle)) * 127);
-            //System.out.println("" + sin[i]);
-        }
-
-        return sin;
-    }
-
- */
 }
-
