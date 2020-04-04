262f89384b98c2a3da3884380946dbf0933f4526
MikolajDuda <duda.mikoaj@gmail.com>
2020-04-04 21:21:44 +0200

    Change WaveMaker to work with modified SoundMaker, change ifs to switch

--- src/SoundMaker.java
+++ src/SoundMaker.java
@@ -1,64 +1,31 @@
-//public class SoundMaker {
-//}
-
 import javax.sound.sampled.AudioFormat;
 import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.SourceDataLine;
 
 
 public class SoundMaker {
-    public static final int SINE = 0;
-    public static final int SQUARE = 1;
-    public static final int TRIANGLE = 2;
-    public static final int SAWTOOTH = 3;
+    private static int sampleRate = 44100;
 
-    private static int sampleRate = 44100;   //must be >= 4000
-    private static double amplitude = 0.4;
-    private static double frequency = 440;
-    private static int size = 32768;         //buffer size = 2^8
+    public static void playWave(byte[] wave) {
+        final AudioFormat af = new AudioFormat(sampleRate, 16, 1, true, true);
 
-
-    public static void playSound(int waveForm, double frequency) {
-        final AudioFormat af = new AudioFormat(sampleRate, 8, 1, true, true);
         try {
             SourceDataLine line = AudioSystem.getSourceDataLine(af);
             line.open(af);
             line.start();
 
-
-            //TODO:  waveForm = SoundMaker.SINE
-            //TODO: play(line, WaveMaker( waveForm, frequency )
-
-
-            play(line, generateSine(frequency, 1));
+            writeToLine(line, wave);
 
             line.drain();
             line.close();
+
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
-    private static byte[] generateSine(double frequencyOfSignal, int seconds) {
-        // total samples = (duration in second) * (samples per second) * 2
-        byte[] sin = new byte[2 * seconds * sampleRate];
-        double samplingInterval = (double) (sampleRate / (2 * frequencyOfSignal));
-        System.out.println("Sampling Frequency  : " + sampleRate);
-        System.out.println("Frequency of Signal : " + frequencyOfSignal);
-        System.out.println("Sampling Interval   : " + samplingInterval);
-
-        for (int i = 0; i < sin.length; i++) {
-            double angle = (2.0 * Math.PI * i) /(2 * samplingInterval);
-            sin[i] = (byte) ((amplitude * Math.sin(angle)) * 127);
-            //System.out.println("" + sin[i]);
-        }
-
-        return sin;
+    private static void writeToLine(SourceDataLine line, byte[] buffer) {
+        line.write(buffer, 0, buffer.length);
     }
 
-    private static void play(SourceDataLine line, byte[] array) {
-        int length = sampleRate * array.length / 1000;
-        line.write(array, 0, array.length);
-    }
 }
-
--- src/WaveMaker.java
+++ src/WaveMaker.java
@@ -10,40 +10,48 @@ public class WaveMaker {
 
     public static byte[] getWave(int waveForm, double frequency) {
 
-        byte[] wave = new byte[2 * seconds * sampleRate];
+        byte[] wave = new byte[3 * seconds * sampleRate];   // 3 * zeby czas w seconds zgadza sie wtedy, kiedy AudioFormat w SoundMakerze ma sampleSizeInBits = 16
 
         double samplingInterval = sampleRate / frequency;
 
-        if (waveForm == SINE) {
-            for (int i = 0; i < wave.length; i++) {
-                double angle = (2.0 * Math.PI * i) / (samplingInterval);  //TODO: sprawdz, czy wystarczy samplingInterval, czy (2 * samplingInterval)
-                wave[i] = (byte) (amplitude * Math.sin(angle) * 127);
-            }
+        switch(waveForm){
+            default:
+                for (int i = 0; i < wave.length; i++) {
+                    wave[i] = (byte) (0 * 127);
+                }
+                break;
+                
+            case SINE:
+                for (int i = 0; i < wave.length; i++) {
+                    double angle = (2.0 * Math.PI * i) / (2 * samplingInterval);  // 2 * wtedy, kiedy AudioFormat w SoundMakerze ma sampleSizeInBits = 16
+                    wave[i] = (byte) (amplitude * Math.sin(angle) * 127);
+                }
+                break;
 
-        } else if (waveForm == SQUARE) {
+            case SQUARE:
+                for (int i = 0; i < wave.length; i++) {
+                    double angle = (2.0 * Math.PI * i) / (2 * samplingInterval);
 
-            for (int i = 0; i < wave.length; i++) {
-                double angle = (2.0 * Math.PI * i) / (samplingInterval);
+                    if (Math.sin(angle) >= 0.0)
+                        wave[i] = (byte) (amplitude * 0.3 * 127);
+                    else
+                        wave[i] = (byte) (amplitude * -0.3 * 128);
+                }
+                break;
 
-                if (Math.sin(angle) >= 0.0)
-                    wave[i] = (byte) (amplitude * 0.2 * 127);
-                else
-                    wave[i] = (byte) (amplitude * -0.2 * 128);
-            }
+            case TRIANGLE:
+                for (int i = 0; i < wave.length; i++) {
+                    double angle = (2.0 * Math.PI * i) / (2 * samplingInterval);
+                    wave[i] = (byte) ((amplitude * (2.0 / Math.PI) * Math.asin(Math.sin(angle)) * 127));
+                }
+                break;
 
-        } else if (waveForm == TRIANGLE) {
-
-            for (int i = 0; i < wave.length; i++) {
-                double angle = (2.0 * Math.PI * i) / (samplingInterval);
-                wave[i] = (byte) ((amplitude * (2.0 / Math.PI) * Math.asin(Math.sin(angle)) * 127));
-            }
-
-        } else if (waveForm == SAWTOOTH) {  //TODO: spraw, aby sawtooth dzialal!
-
-            for (int i = 0; i < wave.length; i++) {
-                double angle = (2.0 * Math.PI * i) / (samplingInterval);
-                wave[i] = (byte) (amplitude * (2.0 / Math.PI * (angle * Math.PI * (i%(1.0/angle))) -Math.PI/2.0) * 127);
-            }
+            case SAWTOOTH:  //TODO: spraw, aby sawtooth dzialal!
+                for (int i = 0; i < wave.length; i++) {
+                    double angle = (2.0 * Math.PI * i) / (2 * samplingInterval);
+                    wave[i] = (byte) (amplitude * 0.6 * (2.0 / Math.PI * (angle * Math.PI * (i%(1.0/angle))) -Math.PI/2.0) * 127);
+                }
+                break;
 
         }
 
